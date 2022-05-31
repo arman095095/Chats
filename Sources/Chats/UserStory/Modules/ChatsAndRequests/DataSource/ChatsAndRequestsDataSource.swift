2ee68b9466ea42b02cc5c12fb01aa6f95d9ff28d
Арман Чархчян <re-mont1@mail.ru>
2022-05-31 17:00:16 +0400

    fix

---
+++ Sources/Chats/UserStory/Modules/ChatsAndRequests/DataSource/ChatsAndRequestsDataSource.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Арман Чархчян on 31.05.2022.
+//
+
+import UIKit
+
+final class ChatsAndRequestsDataSource: UICollectionViewDiffableDataSource<Sections, Item> {
+    
+}
